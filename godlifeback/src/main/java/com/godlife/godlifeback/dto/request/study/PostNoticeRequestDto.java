46f671ed0dd1afe866e1f5787f47568734057d28
khkim <stakkh4@gmail.com>
2023-11-16 20:15:01 +0900

    공지사항 post request, response 수정

--- godlifeback/src/main/java/com/godlife/godlifeback/dto/request/study/PostNoticeRequestDto.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/dto/request/study/PostNoticeRequestDto.java
@@ -10,5 +10,4 @@ import lombok.Setter;
 public class PostNoticeRequestDto {
     @NotBlank
     private String noticeContent;
-    
 }
--- godlifeback/src/main/java/com/godlife/godlifeback/dto/response/study/PostNoticeResponseDto.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/dto/response/study/PostNoticeResponseDto.java
@@ -6,27 +6,19 @@ import org.springframework.http.ResponseEntity;
 import com.godlife.godlifeback.dto.response.ResponseCode;
 import com.godlife.godlifeback.dto.response.ResponseDto;
 import com.godlife.godlifeback.dto.response.ResponseMessage;
-import com.godlife.godlifeback.entity.StudyNoticeEntity;
 
 import lombok.Getter;
 
 @Getter
 public class PostNoticeResponseDto extends ResponseDto{
     
-    private int studyNoticeNumber;
-    private int studyNumber;
-    private String noticeContent;
 
-    private PostNoticeResponseDto(String code, String message, StudyNoticeEntity studyNoticeEntity ){
+    private PostNoticeResponseDto(String code, String message){
         super(code, message);
-
-        this.studyNoticeNumber = studyNoticeEntity.getNoticeNumber();
-        this.studyNumber = studyNoticeEntity.getStudyNumber();
-        this.noticeContent = studyNoticeEntity.getNoticeContent();
     }
 
-    public static ResponseEntity<PostNoticeResponseDto> success(StudyNoticeEntity studyNoticeEntity){
-        PostNoticeResponseDto  result = new PostNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, studyNoticeEntity);
+    public static ResponseEntity<PostNoticeResponseDto> success(){
+        PostNoticeResponseDto  result = new PostNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
