ea920b8202be90d41be5d87f9dc48ff309394382
khkim <stakkh4@gmail.com>
2023-11-17 16:48:10 +0900

    유저resultSet 생성

---
+++ godlifeback/src/main/java/com/godlife/godlifeback/repository/resultSet/UserListResultset.java
@@ -0,0 +1,8 @@
+package com.godlife.godlifeback.repository.resultSet;
+
+public interface UserListResultset {
+
+    String getUserGrade();
+    String getProfileImage();
+    String getNickname();  
+} 
