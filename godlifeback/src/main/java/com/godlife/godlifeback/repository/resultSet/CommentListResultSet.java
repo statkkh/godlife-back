4d812a77ddf1f2d7dfa3c3cefb735b917ed2e3f0
khkim <stakkh4@gmail.com>
2023-11-17 16:48:32 +0900

    CommentResultSet 생성

---
+++ godlifeback/src/main/java/com/godlife/godlifeback/repository/resultSet/CommentListResultSet.java
@@ -0,0 +1,9 @@
+package com.godlife.godlifeback.repository.resultSet;
+
+public interface CommentListResultSet {
+    String getUserGrade();
+    String getProfileImage();
+    String getNickname();
+    String getContent();
+    String getWriteDatetime();
+} 
