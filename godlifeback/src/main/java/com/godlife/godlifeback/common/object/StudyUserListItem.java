5089fa1022eaa2394ac6af4dec12241fb2bf6676
khkim <stakkh4@gmail.com>
2023-11-18 17:28:15 +0900

    스터디 유조 리스트 아이템 생성

---
+++ godlifeback/src/main/java/com/godlife/godlifeback/common/object/StudyUserListItem.java
@@ -0,0 +1,32 @@
+package com.godlife.godlifeback.common.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.godlife.godlifeback.entity.StudyUserListEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudyUserListItem {
+    private int studyNumber;   
+    private String studyUserEmail;
+    private String studyGrade;
+
+    public StudyUserListItem(StudyUserListEntity studyUserListEntity){
+        this.studyNumber = studyUserListEntity.getStudyNumber();
+        this.studyUserEmail = studyUserListEntity.getStudyUserEmail();
+        this.studyGrade = studyUserListEntity.getStudyGrade();
+    }
+
+    public static List<StudyUserListItem> getUserList(List<StudyUserListEntity> studyUserListEntities){
+        List<StudyUserListItem> userList = new ArrayList<>();
+        for(StudyUserListEntity studyUserListEntity : studyUserListEntities){
+            StudyUserListItem studyUserListItem = new StudyUserListItem(studyUserListEntity);
+            userList.add(studyUserListItem);
+        }
+        return userList;
+    }
+}
