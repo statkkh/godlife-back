f59652792536d7141877e0fa094c18c96192eab8
khkim <stakkh4@gmail.com>
2023-11-19 10:30:59 +0900

    스터디 유저 리스트 파일명 수정

---
+++ godlifeback/src/main/java/com/godlife/godlifeback/dto/response/study/GetStudyUserListResponseDto.java
@@ -0,0 +1,35 @@
+package com.godlife.godlifeback.dto.response.study;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.godlife.godlifeback.common.object.StudyUserListItem;
+import com.godlife.godlifeback.dto.response.ResponseCode;
+import com.godlife.godlifeback.dto.response.ResponseDto;
+import com.godlife.godlifeback.dto.response.ResponseMessage;
+import com.godlife.godlifeback.entity.StudyUserListEntity;
+
+import lombok.Getter;
+
+@Getter
+public class GetStudyUserListResponseDto extends ResponseDto{
+
+    private List<StudyUserListItem> studyUserList;
+    
+    private GetStudyUserListResponseDto(String code, String message, List<StudyUserListEntity> studyUserListEntities){
+        super(code, message);
+        this.studyUserList = StudyUserListItem.getUserList(studyUserListEntities);
+    }
+
+    public static  ResponseEntity<GetStudyUserListResponseDto> success( List<StudyUserListEntity> studyUserListEntities){
+        GetStudyUserListResponseDto result = new GetStudyUserListResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, studyUserListEntities);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static  ResponseEntity<ResponseDto> notUserList(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER_LIST, ResponseMessage.NOT_EXIST_USER_LIST);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }    
+}
