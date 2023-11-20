9a32f6278b4775f8d894b9fea6850f8c54c9a2c1
khkim <stakkh4@gmail.com>
2023-11-20 09:58:46 +0900

    스터디 참여 정보 엔터티 인터페이스생성

---
+++ godlifeback/src/main/java/com/godlife/godlifeback/repository/resultSet/StudyUserAttendanceListResultset.java
@@ -0,0 +1,8 @@
+package com.godlife.godlifeback.repository.resultSet;
+
+public interface StudyUserAttendanceListResultset {
+    String getStudyNumber();
+    String getUserGrade();
+    String getProfileImage();
+    String getNickname();  
+} 
