d7b8830581099a2b8dcbd2397a9d1b2231b27066
khkim <stakkh4@gmail.com>
2023-11-20 11:14:21 +0900

    스터디 자료 코멘트 레포지토리 생성

--- godlifeback/src/main/java/com/godlife/godlifeback/repository/StudyMaterialCommentRepository.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/repository/StudyMaterialCommentRepository.java
@@ -3,12 +3,17 @@ package com.godlife.godlifeback.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.godlife.godlifeback.entity.StudyMaterialCommentEntity;
+import com.godlife.godlifeback.repository.resultSet.StudyCommentListResultSet;
 
+@Repository
 public interface StudyMaterialCommentRepository extends JpaRepository<StudyMaterialCommentEntity, Integer>{
     
 
     List<StudyMaterialCommentEntity> findByStudyMaterialCommentNumber(Integer studyMaterialCommentNumber);
+
+    List<StudyCommentListResultSet> findByCommentList(Integer studyNumber);
 
 }
