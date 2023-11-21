6a51b96ec3cbd227c7a9db47027d8a0ec22b3ec5
khkim <stakkh4@gmail.com>
2023-11-21 16:21:31 +0900

    post notice 수정완료

--- godlifeback/src/main/java/com/godlife/godlifeback/dto/response/study/PostNoticeResponseDto.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/dto/response/study/PostNoticeResponseDto.java
@@ -1,24 +1,29 @@
 package com.godlife.godlifeback.dto.response.study;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.godlife.godlifeback.common.object.NoticeListItem;
 import com.godlife.godlifeback.dto.response.ResponseCode;
 import com.godlife.godlifeback.dto.response.ResponseDto;
 import com.godlife.godlifeback.dto.response.ResponseMessage;
+import com.godlife.godlifeback.entity.StudyNoticeEntity;
 
 import lombok.Getter;
 
 @Getter
 public class PostNoticeResponseDto extends ResponseDto{
     
+    private List<NoticeListItem> noticeList;
 
-    private PostNoticeResponseDto(String code, String message){
+    private PostNoticeResponseDto(String code, String message, List<StudyNoticeEntity> studyNoticeEntities){
         super(code, message);
     }
 
-    public static ResponseEntity<PostNoticeResponseDto> success(){
-        PostNoticeResponseDto  result = new PostNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    public static ResponseEntity<PostNoticeResponseDto> success(List<StudyNoticeEntity> studyNoticeEntities){
+        PostNoticeResponseDto  result = new PostNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, studyNoticeEntities);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
