617cd37b474da882091e7cb206bb7c91cc0afe01
khkim <stakkh4@gmail.com>
2023-11-21 16:20:56 +0900

    파일 서비스 implement 수정

--- godlifeback/src/main/java/com/godlife/godlifeback/service/implement/FileServiceImplement.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/service/implement/FileServiceImplement.java
@@ -17,26 +17,25 @@ public class FileServiceImplement implements FileService{
 
     @Value("${file.path}")
     private String filePath;
+    
     @Value("${file.url}")
     private String fileUrl;
     
     @Override
     public String upload(MultipartFile file) {
-        // description: 빈 파일인지 검증 //
-        if (file.isEmpty()) return null;
+        
+        if(file.isEmpty()) return null;
 
-        // description: 원본 파일명 //
         String originalFileName = file.getOriginalFilename();
-        // description: 파일 확장자 구하기 //
+
         String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
-        // description: UUID 형식의 임의의 파일명 생성 //
+
         String uuid = UUID.randomUUID().toString();
-        // description: UUID 형식의 파일명 + 확장자로 새로운 파일명 생성 //
+
         String saveFileName = uuid + extension;
-        // description: 저장할 경로 생성 //
+
         String savePath = filePath + saveFileName;
 
-        // description: 파일 저장 //
         try {
             file.transferTo(new File(savePath));
         } catch (Exception exception) {
@@ -44,22 +43,27 @@ public class FileServiceImplement implements FileService{
             return null;
         }
 
-        // description: 파일을 불러올 수 있는 경로 반환 //
         String url = fileUrl + saveFileName;
-        return url;        
+        return url;
     }
 
     @Override
     public Resource getFile(String fileName) {
+        
         Resource resource = null;
 
-        // description: 파일 저장 경로에서 파일명에 해당하는 파일 불러오기 //
         try {
-            resource = new UrlResource("file:" + filePath + fileName);
+            resource = new UrlResource("file:"+ filePath+fileName);
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
         }
+
         return resource;
+
     }
+
+
+    
+
 }
