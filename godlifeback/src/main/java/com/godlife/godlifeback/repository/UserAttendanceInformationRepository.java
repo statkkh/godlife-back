1e022b53678ad5c12208905afa7aa8a7b3e56685
khkim <stakkh4@gmail.com>
2023-11-23 08:41:29 +0900

    유저 참여 정보 레포지토리 생성

--- godlifeback/src/main/java/com/godlife/godlifeback/repository/UserAttendanceInformationRepository.java
+++ godlifeback/src/main/java/com/godlife/godlifeback/repository/UserAttendanceInformationRepository.java
@@ -10,26 +10,26 @@ import com.godlife.godlifeback.entity.UserAttendanceInformationEntity;
 import com.godlife.godlifeback.entity.primaryKey.UserAttendanceInformationListPk;
 import com.godlife.godlifeback.repository.resultSet.UserAttendanceInformationResultset;
 
-@Repository
-public interface UserAttendanceInformationRepository extends JpaRepository<UserAttendanceInformationEntity,UserAttendanceInformationListPk>{
+// @Repository
+// public interface UserAttendanceInformationRepository extends JpaRepository<UserAttendanceInformationEntity,Integer>{
     
-    @Query(
-        value = 
-        "SELECT " + 
-            "UAI.study_number AS study_number, " +
-            "UAI.user_email AS user_email    " +
-            "SUL.study_grade AS study_grade, " +
-            "U.user_profile_image_url AS user_profile_image, " +
-            "U.user_nickname AS nickname " +
-        "FROM user_attendance_information AS UAI " +
-        "INNER JOIN user AS  U   " +
-        "ON UAI.user_email = U.user_email    " +
-        "INNER JOIN study_user_list AS SUL   " +
-        "ON UAI.user_email = SUL.user_email ",
-        nativeQuery =  true
-    )
-    boolean existsByUserEmailAndStudyNumber(String userEmail, Integer studyNumber);
+//     @Query(
+//         value = 
+//         "SELECT " + 
+//             "UAI.study_number AS study_number, " +
+//             "UAI.user_email AS user_email    " +
+//             "SUL.study_grade AS study_grade, " +
+//             "U.user_profile_image_url AS user_profile_image, " +
+//             "U.user_nickname AS nickname " +
+//         "FROM user_attendance_information AS UAI " +
+//         "INNER JOIN user AS  U   " +
+//         "ON UAI.user_email = U.user_email    " +
+//         "INNER JOIN study_user_list AS SUL   " +
+//         "ON UAI.user_email = SUL.user_email ",
+//         nativeQuery =  true
+//     )
+//     boolean existsByUserEmailAndStudyNumber(String userEmail, Integer studyNumber);
     
     
-    List<UserAttendanceInformationResultset> findByStudyAttenanceInformationList(String userEmail, Integer studyNumber);
-}
+//     List<UserAttendanceInformationResultset> findByStudyAttenanceInformationList(String userEmail, Integer studyNumber);
+// }
