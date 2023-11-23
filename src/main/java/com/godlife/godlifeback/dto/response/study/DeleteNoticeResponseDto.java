1f1171c984d90fc152fea5c53ee61457ee7d53fe
khkim <stakkh4@gmail.com>
2023-11-23 17:51:27 +0900

    sign up request , delete notice response,post study response, patch notice response dto,

---
+++ src/main/java/com/godlife/godlifeback/dto/response/study/DeleteNoticeResponseDto.java
@@ -0,0 +1,44 @@
+package com.godlife.godlifeback.dto.response.study;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.godlife.godlifeback.dto.response.ResponseCode;
+import com.godlife.godlifeback.dto.response.ResponseDto;
+import com.godlife.godlifeback.dto.response.ResponseMessage;
+
+import lombok.Getter;
+
+@Getter
+public class DeleteNoticeResponseDto extends ResponseDto{
+    
+    private DeleteNoticeResponseDto(String code, String message){
+        super(code, message);
+    }
+
+    public static ResponseEntity<DeleteNoticeResponseDto> success(){
+        DeleteNoticeResponseDto result = new DeleteNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistNotice(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_NOTICE, ResponseMessage.NOT_EXIST_NOTICE);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistStudy(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXIST_STUDY_ROOM, ResponseMessage.NOT_EXIST_STUDY_ROOM);
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(result);
+    }    
+
+    public static ResponseEntity<ResponseDto> misMatchLeaderEmail(){
+        ResponseDto result = new ResponseDto(ResponseCode.MISMATCH_LEADER_EMAIL, ResponseMessage.MISMATCH_LEADER_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
+ 
