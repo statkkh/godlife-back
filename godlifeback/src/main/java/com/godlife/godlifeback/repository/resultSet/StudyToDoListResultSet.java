34684c72973818f5f98d44767c186475cd263029
statkkh <140682514+statkkh@users.noreply.github.com>
2023-11-23 21:54:00 +0900

    파일 이름 변경

--- godlifeback/src/main/java/com/godlife/godlifeback/repository/resultSet/StudyToDoListResultSet.java
+++
@@ -1,9 +0,0 @@
-package com.godlife.godlifeback.repository.resultSet;
-
-public interface StudyToDoListResultSet {
-    int getStudyListNumber();     
-    int getStudyNumber();
-    String getStudyListContent();
-    boolean getStudyListCheck();
-    // String userEmail();
-}
