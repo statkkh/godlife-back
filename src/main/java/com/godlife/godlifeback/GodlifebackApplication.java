ba273a15b02f4bd3365aac57f563ffeabc9d88dc
khkim <stakkh4@gmail.com>
2023-11-23 17:48:07 +0900

    get study response

---
+++ src/main/java/com/godlife/godlifeback/GodlifebackApplication.java
@@ -0,0 +1,13 @@
+package com.godlife.godlifeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodlifebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodlifebackApplication.class, args);
+	}
+
+}
