484af2f8658b483433e79092e8288b2e88dbbbed
khkim <stakkh4@gmail.com>
2023-11-25 18:04:56 +0900

    유저 참여 정보 : post request /response 생성

---
+++ src/main/java/com/godlife/godlifeback/dto/request/study/PostUserAttendanceInformationRequestDto.java
@@ -0,0 +1,9 @@
+package com.godlife.godlifeback.dto.request.study;
+
+import javax.validation.constraints.NotNull;
+
+public class PostUserAttendanceInformationRequestDto {
+ 
+    @NotNull 
+    private String userAttendanceCheck;
+}
