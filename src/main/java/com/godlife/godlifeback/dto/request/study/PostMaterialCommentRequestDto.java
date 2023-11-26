8381471abe33e3070dfd7594cd15c8be1ea92011
khkim <stakkh4@gmail.com>
2023-11-26 19:30:39 +0900

    material comment : patch, post request

---
+++ src/main/java/com/godlife/godlifeback/dto/request/study/PostMaterialCommentRequestDto.java
@@ -0,0 +1,16 @@
+package com.godlife.godlifeback.dto.request.study;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostMaterialCommentRequestDto {
+    @NotBlank @Size(min = 2)
+    private String studyMaterialCommentContent;
+}
