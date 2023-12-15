47c0f35ac2d3e134809acda7d6b4361dcdf3d422
statkkh <140682514+statkkh@users.noreply.github.com>
2023-12-15 18:11:22 +0900

    study user : entity  추가

---
+++ src/main/java/com/godlife/godlifeback/entity/StudyUserListEntity.java
@@ -0,0 +1,34 @@
+package com.godlife.godlifeback.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import com.godlife.godlifeback.entity.primaryKey.StudyUserListPK;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+// @AllArgsConstructor
+@Entity(name = "study_user_list")
+@Table(name = "study_user_list")
+@IdClass(StudyUserListPK.class)
+public class StudyUserListEntity {
+    @Id
+    private int studyNumber;
+    @Id
+    private String userEmail;
+    private String studyGrade;
+
+    public StudyUserListEntity(int studyNumber, String userEmail, String studyGrade) {
+        this.studyNumber = studyNumber;
+        this.userEmail = userEmail;
+        this.studyGrade = studyGrade;
+    }
+}
